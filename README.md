1abd95c89f60820908b201d91e0710c7b0b80118
Jay Thorat <j.thorat10@gmail.com>
2023-02-15 11:26:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Django_quickstart
+
+A small python package which will help you to quickly start your django project. It will create a django project with all the necessary files and folders in just single command.
+
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+
+## Run Locally
+
+Install dependencies
+
+```bash
+  py -m pip install Django
+```
+
+Clone the project
+
+```bash
+  git clone https://github.com/jaythorat/django_quickstart
+```
+
+Go to the project directory
+
+```bash
+  cd django_quickstart
+```
+
+Execute the command
+
+```bash
+  py django_quickstart.py project_name application_name
+```
+
+This will create a working django application with all the initial setup which is needed to run the app. Just change CD to project directory and run `py manage.py runserver`
+
+
+## Authors
+
+- [@jaythorat](https://www.github.com/jaythorat)
---
+++ src/django_quickstart_package/django_quickstart.py
@@ -0,0 +1,131 @@
+import sys
+import os
+
+base_path = os.getcwd()
+project_name = sys.argv[1]
+application_name = sys.argv[2]
+def create_directories():
+    os.system(f'django-admin startproject {project_name}')
+    os.chdir(f'{project_name}')
+    os.system(f'django-admin startapp {application_name}')
+    os.system("mkdir templates")
+    os.system("mkdir static")
+    os.system("mkdir media")
+
+# Path: main.py
+def create_files():
+    path_forHTML = f'{os.getcwd()}/templates'
+    path_forCSS = f'{os.getcwd()}/static'
+    html_file = 'index.html'
+    css_file = 'style.css'
+
+    # For HTML file in templates
+    with open(os.path.join(path_forHTML, html_file), 'w') as fp:
+        content = """<!DOCTYPE html>  
+    <html lang="en">  
+    <head>  
+        <meta charset="UTF-8">  
+        <title>Djaango Starter Package</title>
+        {% load static%}
+        <link rel="stylesheet" href="{% static 'style.css' %}">
+    </head>  
+    <body>  
+    <h2>Welcome to Django Starter!!!</h2>  
+    </body>  
+    </html>  
+        """
+        fp.write(content)
+
+    # For CSS file in static
+    with open(os.path.join(path_forCSS, css_file), 'w') as fp:
+        content = """body{
+        background-color: #f1f1f1;
+    }
+        """
+        fp.write(content)
+    
+
+    # For URLS.PY file in project/aplication
+    path_forURLsPY = f'{os.getcwd()}/{application_name}'
+    with open(os.path.join(path_forURLsPY, 'urls.py'), 'w') as fp:
+        content = f"""from django import views
+from django.urls import path
+from {application_name} import views
+import os
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+"""
+        fp.write(content)
+
+    # For VIEWS.PY file in project/aplication
+    path_forViewsPY = f'{os.getcwd()}/{application_name}'   
+    with open(os.path.join(path_forViewsPY, 'views.py'), 'w') as fp:
+        content = f"""from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+"""
+        fp.write(content)
+    
+
+    # For settings.py file in project
+    path_forSettingsPY = f'{os.getcwd()}/{project_name}' 
+    with open(os.path.join(path_forSettingsPY, "settings.py"), 'a') as fp:
+        content = f"""#Added via Django Starter Module for static files
+STATICFILES_DIRS = [
+     BASE_DIR / "static"
+]
+"""
+        fp.write(content)
+        
+    with open(os.path.join(path_forSettingsPY, "settings.py"), 'r+') as fp:
+        content = fp.read()
+        list = content.split("\n")
+        for i in list:
+            if "INSTALLED_APPS" in i:
+                list.insert(list.index(i)+1, f"    '{application_name}.apps.{application_name.capitalize()}Config',")
+            
+            if "TEMPLATES = [" in i:
+                template_dir = list.index(i)+3
+                list[template_dir]=f"        'DIRS': [BASE_DIR / 'templates'],"
+
+        print(len(list))
+        result = ""
+        for i in range(len(list)):
+            result += list[i]
+            result += "\n"
+        fp.seek(0)
+        fp.write(result)
+        # print(result)
+    
+    # For urls.py file in project
+    path_forURLsPY_project = f'{os.getcwd()}/{project_name}'
+    with open(os.path.join(path_forURLsPY_project, "urls.py"), 'r+') as fp:
+        content = fp.read()
+        list = content.split("\n")
+        for i in list:
+            if "from django.contrib import admin" in i:
+                list.insert(list.index(i)+1, f"from django.urls import include")
+            if "urlpatterns = [" in i:
+                list.insert(list.index(i)+1, f"    path('', include('{application_name}.urls')),")
+
+        print(len(list))
+        result = ""
+        for i in range(len(list)):
+            result += list[i]
+            result += "\n"
+        fp.seek(0)
+        fp.write(result)
+        # print(result)
+
+def django_quickstart():
+    create_directories()
+    create_files()
+
+
+
+if __name__ == '__main__':
+    django_quickstart()
